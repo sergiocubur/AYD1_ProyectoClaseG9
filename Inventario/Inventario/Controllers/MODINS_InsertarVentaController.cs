4993d6452909a92d4dac99f09c532b602b289f75
Rodolfo <rochofo@gmail.com>
2019-08-15 00:09:15 -0600

    Historia Ingreso por Venta

---
+++ Inventario/Inventario/Controllers/MODINS_InsertarVentaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pruebas_P1_AyD1.Controllers
+{
+    public class MODINS_InsertarVentaController : Controller
+    {
+        // GET: MODINS_InsertarVenta
+        public ActionResult vMODINS_InsertarVenta()
+        {
+            return View();
+        }
+    }
+}
