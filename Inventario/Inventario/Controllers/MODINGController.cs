3f0ccc77662907be9359d2831131ac7a6b78e077
Ludwin <LudwinBurrion@gmail.com>
2019-08-20 09:02:58 -0600

    Creacion de controllador y vista del modulo de Ingreso

---
+++ Inventario/Inventario/Controllers/MODINGController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventario.Controllers
+{
+    public class MODINGController : Controller
+    {
+        // GET: MODING
+        public ActionResult Devolucion()
+        {
+            return View();
+        }
+    }
+}
