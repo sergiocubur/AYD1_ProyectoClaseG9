35019b5d83eb24a24296ad72599da201001e2965
Huriel <hurieluv2g2d93@gmail.com>
2019-08-21 21:08:57 -0600

    Creacion del Controller y View para el reporte de movimientos

---
+++ Inventario/Inventario/Controllers/MODREP_RepMovimientosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventario.Controllers
+{
+    public class MODREP_RepMovimientosController : Controller
+    {
+        // GET: MODREP_RepMovimientos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
