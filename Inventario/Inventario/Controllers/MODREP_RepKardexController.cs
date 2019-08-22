ca69b9288388e17249e532afdea2a16273098166
Huriel <hurieluv2g2d93@gmail.com>
2019-08-22 12:10:19 -0600

    Edicion de las interfaces para los reportes de compras y movimientos

--- Inventario/Inventario/Controllers/MODREP_RepKardexController.cs
+++ Inventario/Inventario/Controllers/MODREP_RepKardexController.cs
@@ -9,17 +9,31 @@ namespace Inventario.Controllers
     public class MODREP_RepKardexController : Controller
     {
         // GET: MODREP_RepKardex
-        public ActionResult vMODREP_RepKardex() //(List<Objetos.ObjRepKardex> lista)
+        public ActionResult vMODREP_RepKardex()
+        {
+            return View();
+        }
+        public ActionResult mostrandoRepKardex(DateTime fechaIni, DateTime fechaFin)
+        {
+            List<Objetos.ObjRepKardex> listaKardex = getKardex(fechaIni, fechaFin);
+            Session["LOG_KARDEX"] = listaKardex;
+            return RedirectToAction("vMODREP_RepKardex", "MODREP_RepKardex");
+        }
+        [HttpGet]
+        public List<Objetos.ObjRepKardex> getKardex(DateTime fechaIni, DateTime fechaFin)
         {
             List<Objetos.ObjRepKardex> lista = new List<Objetos.ObjRepKardex>();
             Objetos.ObjRepKardex uno = new Objetos.ObjRepKardex("01/01/2019", "Metros", 100, 0, 20.50, "-");
             Objetos.ObjRepKardex dos = new Objetos.ObjRepKardex("02/02/2019", "Martillos", 100, 0, 20.50, "-");
             Objetos.ObjRepKardex tres = new Objetos.ObjRepKardex("-", "Metros", 0, 25, 25.75, "03/03/2019");
+            Objetos.ObjRepKardex cuatro = new Objetos.ObjRepKardex("-", "Metros", 0, 25, 25.75, fechaIni.ToShortDateString());
+            Objetos.ObjRepKardex cinco = new Objetos.ObjRepKardex("-", "Metros", 0, 25, 25.75, fechaFin.ToShortDateString());
             lista.Add(uno);
             lista.Add(dos);
             lista.Add(tres);
-
-            return View(lista);
+            lista.Add(cuatro);
+            lista.Add(cinco);
+            return lista;
         }
     }
 }
--- Inventario/Inventario/Controllers/MODREP_RepMovimientosController.cs
+++ Inventario/Inventario/Controllers/MODREP_RepMovimientosController.cs
@@ -9,9 +9,40 @@ namespace Inventario.Controllers
     public class MODREP_RepMovimientosController : Controller
     {
         // GET: MODREP_RepMovimientos
-        public ActionResult Index()
+        public ActionResult vMODREP_RepMovimientos()
         {
             return View();
+        }
+
+
+        public ActionResult mostrandoRepMovimientos(DateTime fechaIni, DateTime fechaFin, string tipoMovimiento)
+        {
+            List<Objetos.ObjRepKardex> listaMovimientos = getMovimientos(fechaIni, fechaFin, tipoMovimiento);
+            Session["LOG_MOVIMIENTOS"] = listaMovimientos;
+            return RedirectToAction("vMODREP_RepMovimientos", "MODREP_RepMovimientos");
+        }
+        [HttpGet]
+        public List<Objetos.ObjRepKardex> getMovimientos(DateTime fechaIni, DateTime fechaFin, string tipoMovimiento)
+        {
+            List<Objetos.ObjRepKardex> lista = new List<Objetos.ObjRepKardex>();
+            if (tipoMovimiento.Equals("in"))    //Reporte de entradas
+            {
+                Objetos.ObjRepKardex uno = new Objetos.ObjRepKardex("01/01/2019", "in", 100, 0, 20.50, "in");
+                Objetos.ObjRepKardex dos = new Objetos.ObjRepKardex("02/02/2019", "in", 100, 0, 20.50, "in");
+                lista.Add(uno);
+                lista.Add(dos);
+            }
+            else//Reporte de salidas
+            {
+                Objetos.ObjRepKardex tres = new Objetos.ObjRepKardex("out", "Metros", 0, 25, 25.75, "03/03/2019");
+                Objetos.ObjRepKardex cuatro = new Objetos.ObjRepKardex("salida", "Metros", 0, 25, 25.75, fechaIni.ToShortDateString());
+                Objetos.ObjRepKardex cinco = new Objetos.ObjRepKardex("salida", "Metros", 0, 25, 25.75, fechaFin.ToShortDateString());
+                lista.Add(tres);
+                lista.Add(cuatro);
+                lista.Add(cinco);
+            }
+
+            return lista;
         }
     }
 }
