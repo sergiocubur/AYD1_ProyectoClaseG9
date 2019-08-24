7eca4999d3ed3e1e54b02c7415b3c20dd1929c57
Ludwin <LudwinBurrion@gmail.com>
2019-08-23 23:17:37 -0600

    Creacion del objeto para devoluciones e ingreso de devoluciones de muestra

--- Inventario/Inventario/Controllers/MODINGController.cs
+++ Inventario/Inventario/Controllers/MODINGController.cs
@@ -11,7 +11,14 @@ namespace Inventario.Controllers
         [HttpGet]
         public ActionResult Devoluciones()
         {
-            return View();
+            List<Objetos.ObjDevolucion> devoluciones = new List<Objetos.ObjDevolucion>();
+            Objetos.ObjDevolucion devolucion1 = new Objetos.ObjDevolucion(1,"V7-999827","10/08/2019","Ingreso por mal estado","I","Ludwin");
+            Objetos.ObjDevolucion devolucion2 = new Objetos.ObjDevolucion(2,"A9-398889","15/08/2019", "Ingreso por mal estado", "I", "Marcos");
+            Objetos.ObjDevolucion devolucion3 = new Objetos.ObjDevolucion(3,"J8-929909", "16/08/2019", "Ingreso por cambio de producto", "I", "Monica");
+            devoluciones.Add(devolucion1);
+            devoluciones.Add(devolucion2);
+            devoluciones.Add(devolucion3);
+            return View(devoluciones);
         }
 
         [HttpGet]
