dcd94e66023fbe6c5b707ef64bb2e9a51edeb230
Sergio Cubur <sergiocubur@gmail.com>
2019-08-25 19:07:17 -0600

    Modulo Productos

---
+++ Inventario/Inventario/Controllers/PruebaDataBaseController.cs
@@ -0,0 +1,20 @@
+﻿using Inventario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventario.Controllers
+{
+    public class PruebaDataBaseController : Controller
+    {
+        // GET: PruebaDataBase
+        public ActionResult Index()
+        {
+            DataBase db = new DataBase();
+            var listusuarios=db.producto.ToList();
+            return new JsonResult { JsonRequestBehavior = JsonRequestBehavior.AllowGet, Data = listusuarios };
+        }
+    }
+}
