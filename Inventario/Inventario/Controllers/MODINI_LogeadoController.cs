c74df495cc0aa62b6ca288d4a1630a4dd14601b3
huriel2g <hurieluv2g2d93@gmail.com>
2019-09-15 00:14:40 -0600

    Pruebas bdd Agregando Interfaz de Login y Menu Principal Modificando Modulo Reportes Bloqueando usuarios no logueados

---
+++ Inventario/Inventario/Controllers/MODINI_LogeadoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventario.Controllers
+{
+    public class MODINI_LogeadoController : Controller
+    {
+        // GET: MODINI_Logeado
+        public ActionResult MODINI_Logeado()
+        {
+            return View();
+        }
+    }
+}
---
+++ Inventario/Inventario/Controllers/MODINI_LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventario.Controllers
+{
+    public class MODINI_LoginController : Controller
+    {
+        // GET: MODINI_Login
+        public ActionResult vMODINI_Login()
+        {
+            return View();
+        }
+    }
+}
