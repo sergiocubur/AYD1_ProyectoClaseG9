cda1bdcb5f56b4225be2f02b433d878f073940f5
huriel2g <hurieluv2g2d93@gmail.com>
2019-09-15 17:22:27 -0600

    Agregando codigo que solucione las pruebas para el login BDD

--- Inventario/Inventario/Controllers/MODINI_LogeadoController.cs
+++ Inventario/Inventario/Controllers/MODINI_LogeadoController.cs
@@ -9,7 +9,7 @@ namespace Inventario.Controllers
     public class MODINI_LogeadoController : Controller
     {
         // GET: MODINI_Logeado
-        public ActionResult MODINI_Logeado()
+        public ActionResult vMODINI_Logeado()
         {
             return View();
         }
