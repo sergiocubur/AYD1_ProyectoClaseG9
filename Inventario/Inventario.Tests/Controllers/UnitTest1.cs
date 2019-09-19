e8c84cdf3d825196b109e225f27c3cebcadf610d
Rodolfo <rochofo@gmail.com>
2019-09-19 13:37:50 -0600

    Agregando pruebas unitarias Resta en Mal Estado

---
+++ Inventario/Inventario.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Inventario.Tests.Controllers
+{
+    [TestClass]
+    public class TestRestarME
+    {
+        [TestMethod]
+        public void TestRestaCero()
+        {
+            var rme = new MODRES_MEstado();
+
+            var result = rme.consulta("producto1", 0);
+
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void TestRestaSinNombre()
+        {
+            var rme = new MODRES_MEstado();
+
+            var result = rme.consulta("", 1);
+
+            Assert.AreEqual(result, 1);
+        }
+    }
+}
