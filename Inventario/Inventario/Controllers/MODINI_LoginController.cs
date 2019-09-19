7a3610071931e925304c484fc70a84f97f4c196a
LudwinGit <ludwinburrion@gmail.com>
2019-09-19 14:55:30 -0600

    Regresando la cadena de conexion original

--- Inventario/Inventario/Controllers/MODINI_LoginController.cs
+++ Inventario/Inventario/Controllers/MODINI_LoginController.cs
@@ -49,8 +49,7 @@ namespace Inventario.Controllers
 
         public static DataTable consultarBD(string Consulta)
         {
-            //string credenciales = "server=LAPTOP-SCUBUR\\SQLEXPRESS02; database=AnalisisP1 ; integrated security = true";
-            string credenciales = "server=DESARROLLO-LAP; database=AnalisisP1 ; integrated security = true";
+            string credenciales = "server=LAPTOP-SCUBUR\\SQLEXPRESS02; database=AnalisisP1 ; integrated security = true";
             SqlConnection conexion = new SqlConnection(credenciales);
             SqlDataAdapter adaptador = new SqlDataAdapter();
             DataTable ds = new DataTable();
