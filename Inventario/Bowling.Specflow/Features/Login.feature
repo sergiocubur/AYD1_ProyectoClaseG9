b7037b44f77f1a565a404b2545c6d7d0ee80fad2
Sergio Cubur <sergiocubur@gmail.com>
2019-09-19 16:26:38 -0600

    BDD para Login Ok  Scenario 1

---
+++ Inventario/Bowling.Specflow/Features/Login.feature
@@ -0,0 +1,10 @@
+﻿Feature: Login
+
+@mytag
+Scenario: Ok Login
+	Given Lunch navigator Chrome
+	And Navigate to localhost 
+	When Enter code user 2626
+	When enter Password 2628
+	And Click on Login Button
+	Then Result should be visible home page
---
+++ Inventario/Bowling.Specflow/StepDefinitions/LoginSteps.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace Bowling.Specflow.StepDefinitions
+{
+    [Binding]
+    public class LoginSteps
+    {
+        public IWebDriver driver;
+        public int mil = 2000;
+
+        [Given(@"Lunch navigator Chrome")]
+        public void GivenLunchNavigatorChrome()
+        {
+            //lauch brouser
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService(@"E:\", "chromedriver.exe");
+            driver = new ChromeDriver(service);
+
+            
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMinutes(1);
+        }
+        
+        [Given(@"Navigate to localhost")]
+        public void GivenNavigateToLocalhost()
+        {
+
+            driver.Navigate().GoToUrl("http://localhost:3252/");
+            
+            Thread.Sleep(mil);
+        }
+        
+        [When(@"Enter code user (.*)")]
+        public void WhenEnterCodeUser(int p0)
+        {
+            driver.FindElement(By.Id("user")).SendKeys("2626");
+            
+            Thread.Sleep(mil);
+        }
+        
+        [When(@"enter Password (.*)")]
+        public void WhenEnterPassword(int p0)
+        {
+            driver.FindElement(By.Id("password")).SendKeys("2626");
+            
+            Thread.Sleep(mil);
+        }
+        
+        [When(@"Click on Login Button")]
+        public void WhenClickOnLoginButton()
+        {
+            driver.FindElement(By.Id("btn")).Click();
+            
+            Thread.Sleep(mil);
+        }
+        
+        [Then(@"Result should be visible home page")]
+        public void ThenResultShouldBeVisibleHomePage()
+        {
+            driver.Quit();
+        }
+    }
+}
