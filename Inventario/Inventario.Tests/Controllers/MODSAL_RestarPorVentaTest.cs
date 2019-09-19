205675d4eef444d524f03955777ded65a203b803
huriel2g <hurieluv2g2d93@gmail.com>
2019-09-19 16:07:51 -0600

    Agregando pruebas a historia RestarPorVentas del modulo de salidas

--- Inventario/Inventario.Tests/Controllers/MODSAL_RestarPorVentaTest.cs
+++ Inventario/Inventario.Tests/Controllers/MODSAL_RestarPorVentaTest.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Inventario;
 using Inventario.Controllers;
+using Inventario.Objetos;
+using System.Data;
 
 namespace Inventario.Tests.Controllers
 {
@@ -44,6 +46,36 @@ namespace Inventario.Tests.Controllers
         {
             MODSAL_RestarPorVentaTest controller = new MODSAL_RestarPorVentaTest();
             ViewResult result = null;// = controller.Index() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void testRetornoDeLaVista()
+        {
+            // Arrange
+            MODSAL_RestarPorVentaTest controller = new MODSAL_RestarPorVentaTest();
+            // Act
+            ViewResult result = null;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void testLlenandoListaParaMostrarVentas()
+        {
+            // Arrange
+            MODSAL_RestarPorVentaTest controller = new MODSAL_RestarPorVentaTest();
+            // Act
+            List<ObjRestaxVenta> result = null;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void testConsultandoLaBD()
+        {
+            // Act
+            DataTable result = null;
             // Assert
             Assert.IsNotNull(result);
         }
