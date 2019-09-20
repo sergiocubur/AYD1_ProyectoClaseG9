8a9e180aa3d40969a7df5e0bcc04fdeb131e07cf
huriel2g <hurieluv2g2d93@gmail.com>
2019-09-19 23:04:59 -0600

    Agregando codigo para prueba Retorno De Vista Historia de usuario Restar Por Venta TDD

--- Inventario/Inventario.Tests/Controllers/MODSAL_RestarPorVentaTest.cs
+++ Inventario/Inventario.Tests/Controllers/MODSAL_RestarPorVentaTest.cs
@@ -6,76 +6,17 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Inventario;
 using Inventario.Controllers;
-using Inventario.Objetos;
-using System.Data;
 
 namespace Inventario.Tests.Controllers
 {
     [TestClass]
     public class MODSAL_RestarPorVentaTest
     {
-        [TestMethod]
+       [TestMethod]
         public void RetornoDeVista()
         {
-            MODSAL_RestarPorVentaTest controller = new MODSAL_RestarPorVentaTest();
-            ViewResult result = null;// = controller.Index() as ViewResult;
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void RestarProductoCantidad0()
-        {
-            MODSAL_RestarPorVentaTest controller = new MODSAL_RestarPorVentaTest();
-            ViewResult result = null;// = controller.Index() as ViewResult;
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void RestarProductoSinNombreNiCantidad()
-        {
-            MODSAL_RestarPorVentaTest controller = new MODSAL_RestarPorVentaTest();
-            ViewResult result = null;// = controller.Index() as ViewResult;
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void RestarCantidadMayorAExistente()
-        {
-            MODSAL_RestarPorVentaTest controller = new MODSAL_RestarPorVentaTest();
-            ViewResult result = null;// = controller.Index() as ViewResult;
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void testRetornoDeLaVista()
-        {
-            // Arrange
-            MODSAL_RestarPorVentaTest controller = new MODSAL_RestarPorVentaTest();
-            // Act
-            ViewResult result = null;
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void testLlenandoListaParaMostrarVentas()
-        {
-            // Arrange
-            MODSAL_RestarPorVentaTest controller = new MODSAL_RestarPorVentaTest();
-            // Act
-            List<ObjRestaxVenta> result = null;
-            // Assert
-            Assert.IsNotNull(result);
-        }
-        [TestMethod]
-        public void testConsultandoLaBD()
-        {
-            // Act
-            DataTable result = null;
+            MODSAL_RestarPorVentaController controller = new MODSAL_RestarPorVentaController();
+            ViewResult result = controller.vMODSAL_RestarPorVenta() as ViewResult;
             // Assert
             Assert.IsNotNull(result);
         }
---
+++ Inventario/Inventario/Controllers/MODSAL_RestarPorVentaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventario.Controllers
+{
+    public class MODSAL_RestarPorVentaController : Controller
+    {
+        // GET: MODSAL_RestarPorVenta
+        public ActionResult vMODSAL_RestarPorVenta()
+        {
+            return View();
+        }
+    }
+}
