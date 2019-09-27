8a51a7cc68a41ed3597ae93708239d0a939200f8
Rodolfo <rochofo@gmail.com>
2019-09-26 22:59:35 -0600

    Agregando select para cantidad de productos

--- Inventario/Inventario/MODSAL_RestaMalEstado.aspx.cs
+++ Inventario/Inventario/MODSAL_RestaMalEstado.aspx.cs
@@ -64,39 +64,38 @@ namespace Inventario
             }
             else
             {
-                string credenciales = "server=RODOLFO-HP\\SQL2017;database=Practica2;integrated security=true";
-                SqlConnection con = new SqlConnection(credenciales);
-                SqlCommand command = new SqlCommand();
-
-                command.Connection = con;
-                command.CommandType = CommandType.Text;
-                command.CommandText = "SELECT id FROM producto where nombre=@nimbre";
-                command.Parameters.Add("@nombre", SqlDbType.VarChar).Value = txtProducto.Text;
-                /*command.Parameters.Add("@dpi", SqlDbType.VarChar).Value = txtdpi.Text;
-                command.Parameters.Add("@nombre", SqlDbType.VarChar).Value = txtNombre.Text;
-                command.Parameters.Add("@dir", SqlDbType.VarChar).Value = txtDireccion.Text;
-                command.Parameters.Add("@mail", SqlDbType.VarChar).Value = txtCorreo.Text;
-                command.Parameters.Add("@cod", SqlDbType.VarChar).Value = txtUsuario.Text;
-                command.Parameters.Add("@pass", SqlDbType.VarChar).Value = txtPassword.Text;*/
-
-                try
-                {
-                    con.Open();
-                    //Response.Write("Conexion Establecida");
-                    int ok = command.ExecuteNonQuery();
-                    con.Close();
-
-                    if (ok > 0)
-                    {
-                        return 2;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.Write(ex);
-                }
-                return 0;
+                int c = consultaSelect(product);
             }
+            return 0;
+        }
+
+        public int consultaSelect(string product)
+        {
+            int cant = Convert.ToInt32(txtCantidad.Text);
+            string credenciales = "server=RODOLFO-HP\\SQL2017;database=Practica2;integrated security=true";
+            SqlConnection con = new SqlConnection(credenciales);
+            SqlCommand command = new SqlCommand();
+
+            command.Connection = con;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT cantidad FROM producto WHERE descripcion=@product";
+            command.Parameters.AddWithValue("@product", txtProducto.Text);
+
+            try
+            {
+                con.Open();
+                int a = Convert.ToInt32(command.ExecuteScalar());
+                if (a != 0)
+                {
+                    return 1;
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+            }
+            return 0;
         }
     }
 }
