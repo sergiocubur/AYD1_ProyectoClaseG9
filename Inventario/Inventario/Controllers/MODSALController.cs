270fb875ed532847cf4ef231d36dd2f575468524
huriel2g <hurieluv2g2d93@gmail.com>
2019-11-06 13:32:18 -0600

    Agregando Submenu Salidas

---
+++ Inventario/Inventario/Controllers/MODSALController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventario.Controllers
+{
+    public class MODSALController : Controller
+    {
+        // GET: MODSAL
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult vRestarMalEstado()
+        {
+            return View();
+        }
+    }
+}
